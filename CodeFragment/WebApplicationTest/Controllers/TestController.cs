d60bcd28529c0feb16569a6dd30c4789e825abef
sunnyluck <447102968@qq.com>
2018-04-04 16:03:58 +0800

    MVC回忆

---
+++ CodeFragment/WebApplicationTest/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationTest.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+
+        public string GetString()
+        {
+            return "Hello World is old now. It’s time for wassup bro ;)";
+        }
+    }
+}
